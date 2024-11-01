bc4678d023804e49a724d9d0e4fa7606bf0074ee
Shakaet <abdshakaet@gmail.com>
2024-11-02 01:17:40 +0600

    adding

--- src/Component/ListTedBook/ListTedBook.jsx
+++ src/Component/ListTedBook/ListTedBook.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import { getItemFromLs } from '../AddToDb';
+import Book from '../Book/Book';
 
 const ListTedBook = () => {
 
+    let [readData,setReadData]=useState([])
+
     let data= useLoaderData()
 
-    console.log(data.length)
+    useEffect(()=>{
+        let getLsData=getItemFromLs()
+
+        console.log(data,getLsData)
+
+        let dataInt= getLsData.map(id=>parseInt(id))
+
+        console.log(typeof dataInt)
+
+        let readBookList= data.filter(item=>dataInt.includes(item.bookId))
+
+        setReadData(readBookList)
+    },[])
+
+    
     return (
         <div>
 
@@ -20,7 +38,13 @@ const ListTedBook = () => {
                 </TabList>
 
                 <TabPanel>
-                <h2>Any content 1</h2>
+                <h2 className='text-center mb-10'>Total Read Books:{readData.length}</h2>
+
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {
+                    readData.map(book=><Book book={book}></Book>)
+                }
+                </div>
                 </TabPanel>
                 <TabPanel>
                 <h2>Any content 2</h2>
